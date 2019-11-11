c9c838a913bf548d7fd224648a9fa32699bd750b
mitchelnieuwendijk <mnieuwendijk01@gmail.com>
2019-11-11 14:13:45 +0100

    Removed views

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="row">
-        <div class="col-md-6 mx-auto text-center pt-5">
-            <p class="h1">Welcome</p>
-        </div>
-    </div>
-@endsection
