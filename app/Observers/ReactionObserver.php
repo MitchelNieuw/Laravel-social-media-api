8fd56ff7b4bc44fe19fe0ba59c4b4f40100a0155
mitchelnieuwendijk <mnieuwendijk01@gmail.com>
2019-11-16 20:19:17 +0100

    Update

--- app/Observers/ReactionObserver.php
+++ app/Observers/ReactionObserver.php
@@ -7,6 +7,7 @@ use App\Notifications\UserReactionNotification;
 use App\Reaction;
 use App\Repositories\NotificationRepository;
 use App\Repositories\UserRepository;
+use App\User;
 
 /**
  * @package App\Observers
@@ -26,25 +27,29 @@ class ReactionObserver
      */
     private function notifyUserOfMessage(Reaction $reaction): void
     {
-        $user = (new UserRepository())->getUserById($reaction->getAttribute('user_id'));
+        $authenticatedUser = (new UserRepository())->getUserById($reaction->getAttribute('user_id'));
+        $userForNotification =  (new UserRepository())->getUserById($reaction->message->user_id);
         if (
-            ($user !== null)
-            && $this->checkSendNotificationIsPossible($reaction, $user->getAttribute('id')) !== null
+            $authenticatedUser !== null
+            && $userForNotification !== null
+            && $this->checkSendNotificationIsPossible($userForNotification, $authenticatedUser) !== null
         ) {
-            $user->notify(new UserReactionNotification($reaction->user->tag, $reaction->getAttribute('message_id')));
+            $userForNotification->notify(
+                new UserReactionNotification($reaction->user->tag, $reaction->getAttribute('message_id'))
+            );
         }
     }
 
     /**
-     * @param Reaction $reaction
-     * @param int $authenticatedUserId
+     * @param User $userForNotification
+     * @param User $authenticatedUser
      * @return Follow|null
      */
-    private function checkSendNotificationIsPossible(Reaction $reaction, int $authenticatedUserId): ?Follow
+    private function checkSendNotificationIsPossible(User $userForNotification, User $authenticatedUser): ?Follow
     {
         return (new NotificationRepository())->checkNotificationsAreTurnedOnForAuthenticatedUser(
-            $reaction->message->user_id,
-            $authenticatedUserId
+            $authenticatedUser->getAttribute('id'),
+            $userForNotification->getAttribute('id')
         );
     }
 }
